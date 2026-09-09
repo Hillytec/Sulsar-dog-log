457bf81e2c75ef8cbd2203136654d4fe5a5e45f5
Hillytec <hilltec@hotmail.com>
2026-09-09 20:15:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'k9fieldlog-v9';
+var CACHE = 'k9fieldlog-v10';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', function(e){
